2a4ffce8fe58aa64baeebcad75acbf1124865b71
helionporto <helionporto@hotmail.com>
2020-12-29 16:27:13 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const MercadoPago = require('mercadopago');
+
+const app = express();
+
+MercadoPago.configure({
+    sandbox: true,
+    access_token: "TEST-1715245730601227-122813-0cd0d877d9c9012e519cc5f334319acd-46494753"
+});
+
+app.get('/', (req,res) => {
+    res.send("Olá mundo!");
+});
+
+app.get('/pagar', async (req,res) => {
+
+    var id = ""+Date.now();
+    var dados={
+        items: [
+            item={
+                id: id,
+                title: "Camisa social de algodão",
+                quantity: 1,
+                currency_id: "BRL",
+                unit_price: parseFloat(151,20)
+            }
+        ],
+        payer: {
+            email:"helionporto@hotmail.com"
+        },
+        external_reference: id 
+
+    }
+
+    try {
+        var payment = await MercadoPago.preferences.create(dados);
+        console.log(payment);
+        return res.redirect(payment.body.init_point);
+    } catch (error) {
+        return res.send(error.message);
+    }
+
+   
+});
+
+app.listen(3000,(req, res) =>{
+    console.log("Servidor rodando!");
+});
