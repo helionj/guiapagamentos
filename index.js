f150c9733faf5072e5016195d786098e106fcae6
helionporto <helionporto@hotmail.com>
2021-01-01 19:54:34 -0300

    status do pagamento

--- index.js
+++ index.js
@@ -52,7 +52,11 @@ app.post('/not', (req,res) => {
         MercadoPago.payment.search({
             qs: filtro
         }).then(data =>{
-            console.log(data);
+            var pagamento = data.body.results[0];
+            if(pagamento != undefined){
+                console.log("id: "+pagamento.external_reference);
+                console.log("Status: "+pagamento.status);
+            }
         }).catch(error =>{
             console.log(error);
         });
